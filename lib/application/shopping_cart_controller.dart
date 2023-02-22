c88d5685c70787418f10b924acb7dafb24489a4e
kzawadi <kelvin.vitone@gmail.com>
2023-02-22 15:00:25 +0300

    refctoring a state class

--- lib/application/shopping_cart_controller.dart
+++ lib/application/shopping_cart_controller.dart
@@ -6,11 +6,11 @@ import 'package:tunzaa_market_place_challenge/domain/core/tunzaa_failures.dart';
 import 'package:tunzaa_market_place_challenge/domain/models/shopping_item_model.dart';
 
 /// A main state class of the product catalogue
-/// its a statenotifier with initial value of [AsyncLoading]
+/// its a statenotifier with initial value of [ShopState]
 /// and upon instance creation we envoke a [fetchItems] method
 /// and return an [Either] type of failure [TunzaaFailures] on the left
 /// and a list of ProductItems[ShoppingItemModel]. This ensure that our
-/// apps works functional and no function from services return void
+/// apps works functionally and no function from services return void
 /// {Functional programming}. so we handle every situation according
 
 class ProductNotifier extends StateNotifier<ShopState> {
@@ -19,17 +19,18 @@ class ProductNotifier extends StateNotifier<ShopState> {
   }
   final Ref ref;
 
-  Future fetchItems() async {
+  fetchItems() async {
     final shooppingServicesInterface =
         ref.read(shoppingServicesProviderProvider);
 
     state = state.copyWith(isLoading: true);
 
-    final results = await shooppingServicesInterface.fetchItems();
-    state = results.fold(
-      (l) => state.copyWith(failures: some(l), isLoading: false),
-      (r) => state.copyWith(item: r, isLoading: false),
-    );
+    state = await shooppingServicesInterface.fetchItems().then(
+          (value) => value.fold(
+            (l) => state.copyWith(failures: some(l), isLoading: false),
+            (r) => state.copyWith(item: r, isLoading: false),
+          ),
+        );
   }
 
   Future search(String searchterm) async {
@@ -40,30 +41,34 @@ class ProductNotifier extends StateNotifier<ShopState> {
         ref.read(shoppingServicesProviderProvider);
     state = state.copyWith(isLoading: true);
 
-    final results = await shooppingServicesInterface.fetchItems();
-//todo(kzawadi) a weird behavior happens when you modify a list of a modified list
-//todo(kzawadi) the comented out code is usable b ut opinionated
-    // Convert list to list of movies using the movie class constructor with simple filter title function inside
-    final searchedList =
-        // state.item
-        //     .where((element) =>
-        //         element.title!.toLowerCase().contains(searchterm.toLowerCase()))
-        //     .toList();
-        results.fold(
-      (l) => null,
-      (r) => r
-          .where((element) =>
-              element.title!.toLowerCase().contains(searchterm.toLowerCase()))
-          .toList(),
-    );
-    // var p = state.item;
-    // if (searchterm.isNotEmpty) {
-    //   state = state.copyWith(item: searchedList, isLoading: false);
-    // } else {
-    //   state = state.copyWith(item: p, isLoading: false);
-    // }
+    if (searchterm.isNotEmpty) {
+      final results = await shooppingServicesInterface.fetchItems();
 
-    state = state.copyWith(item: searchedList!, isLoading: false);
+//todo(kzawadi) a weird behavior happens when you modify a list of a modified list
+//todo(kzawadi) the commented out code is usable but opinionated
+      final searchedList =
+          // state.item
+          //     .where((element) =>
+          //         element.title!.toLowerCase().contains(searchterm.toLowerCase()))
+          //     .toList();
+          results.fold(
+        (l) => null,
+        (r) => r
+            .where((element) =>
+                element.title!.toLowerCase().contains(searchterm.toLowerCase()))
+            .toList(),
+      );
+      // var p = state.item;
+      // if (searchterm.isNotEmpty) {
+      //   state = state.copyWith(item: searchedList, isLoading: false);
+      // } else {
+      //   state = state.copyWith(item: p, isLoading: false);
+      // }
+
+      state = state.copyWith(item: searchedList!, isLoading: false);
+    } else {
+      state = state.copyWith(isLoading: false);
+    }
   }
 
   Future addToCart(int id, {required bool inCart}) async {
